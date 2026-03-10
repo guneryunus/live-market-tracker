465df9e93d8d2ce029112b3ecb5de50679d5813d
Emre Guner <emre.guner@demirorenteknoloji.com>
2026-03-10 10:29:07 +0300

    feat: google analytics intergration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
-})
+  modules: ["@nuxtjs/tailwindcss", "nuxt-gtag"],
+  gtag: {
+    id: "G-EXZJBLK1RF",
+  },
+});
